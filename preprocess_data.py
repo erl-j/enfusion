551bb1a3a60c094cc6954acd7ecc9ddf72198c03
root <nicolasjonason@gmail.com>
2022-12-26 13:18:51 +0000

    demo works

--- preprocess_data.py
+++ preprocess_data.py
@@ -83,23 +83,7 @@ for fp in sample_fps:
 
 #%%
 
-class TextEmbedder():
-
-    def __init__(self) -> None:
-        self.tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
-        self.model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-        self.text_to_embeddings = {}
-    
-    def _embed_text(self,string):
-        input_ids = self.tokenizer(string, return_tensors="pt",padding=True).input_ids
-        with torch.no_grad():
-            text_features= self.model.get_text_features(input_ids=input_ids).detach()[0]
-        return text_features
-        
-    def embed_text(self,text):
-        if text not in self.text_to_embeddings:
-            self.text_to_embeddings[text] = self._embed_text(text)
-        return self.text_to_embeddings[text]
+from text_embedder import TextEmbedder
 
 text_embedder = TextEmbedder()
 
